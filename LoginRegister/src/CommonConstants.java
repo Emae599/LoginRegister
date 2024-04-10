b42d7e2835e5a3eca0e5926352eaa8ecbc6fde6b
Emae Calonia <164385828+Emae599@users.noreply.github.com>
2024-04-10 12:19:07 +0800

    CommonConstants.java

--- LoginRegister/src/CommonConstants.java
+++ LoginRegister/src/CommonConstants.java
@@ -11,7 +11,7 @@ public class CommonConstants {
     public static final Color BUTTON_COLOR = new Color(56,182,255);
 
     // frame config
-    public static final Dimension FRAME_SIZE = new Dimension(540, 890);
+    public static final Dimension FRAME_SIZE = new Dimension(540, 780);
     public static final Dimension TEXTFIELD_SIZE = new Dimension((int) (FRAME_SIZE.width * 0.80), 50);
     public static final Dimension BUTTON_SIZE = TEXTFIELD_SIZE;
 
@@ -19,7 +19,7 @@ public class CommonConstants {
     public static final Dimension LOGIN_IMAGE_SIZE = new Dimension(370, 340);
 
     // register config
-    public static final Dimension REGISTER_LABEL_SIZE = new Dimension(FRAME_SIZE.width, 100);
+    public static final Dimension REGISTER_LABEL_SIZE = new Dimension(FRAME_SIZE.width, 130);
 
     // result dialog config
     public static final Dimension RESULT_DIALOG_SIZE = new Dimension((int)(FRAME_SIZE.width/3), (int)(FRAME_SIZE.height/6));
