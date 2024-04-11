cb040b13ad3fff118cb619816016749910db9d57
Emae Calonia <164385828+Emae599@users.noreply.github.com>
2024-04-11 15:30:09 +0800

    UserDB.java

--- LoginRegister/src/UserDB.java
+++ LoginRegister/src/UserDB.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 
 public class UserDB {
-    public static HashMap<String, String> userDB = new HashMap<>();
-    public static  void addUser(String username, String password){userDB.put(username, password);}
+	public static HashMap<String, String> userDB = new HashMap<>();
+	public static  void addUser1(String number, String password){userDB.put(number, password);}
+	public static  void addUser2(String email, String password) {userDB.put(email, password);}
 }
