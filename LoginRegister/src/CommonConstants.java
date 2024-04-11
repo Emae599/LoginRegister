291847c771322cc52d0a74f5fb50e72cd587f88a
Emae Calonia <164385828+Emae599@users.noreply.github.com>
2024-04-11 11:33:44 +0800

    CommonConstants.java

--- LoginRegister/src/CommonConstants.java
+++ LoginRegister/src/CommonConstants.java
@@ -4,15 +4,15 @@ public class CommonConstants {
     // file paths
     public static final String LOGIN_IMAGE_PATH = "resources/Logo.png";
     public static final String FONT_PATH = "resources/coolvetica condensed rg.otf";
-
-    // color config
-    public static final Color PRIMARY_COLOR = new Color(255,255,255);
+    
+	// color config
+    public static final Color PRIMARY_COLOR = new Color(230, 247, 255);
     public static final Color SECONDARY_COLOR = new Color(0,74,173);
     public static final Color BUTTON_COLOR = new Color(56,182,255);
 
     // frame config
-    public static final Dimension FRAME_SIZE = new Dimension(540, 780);
-    public static final Dimension TEXTFIELD_SIZE = new Dimension((int) (FRAME_SIZE.width * 0.80), 50);
+    public static final Dimension FRAME_SIZE = new Dimension(700, 800);
+    public static final Dimension TEXTFIELD_SIZE = new Dimension((int) (FRAME_SIZE.width * 0.85), 50);
     public static final Dimension BUTTON_SIZE = TEXTFIELD_SIZE;
 
     // login config
